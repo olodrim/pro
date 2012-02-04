8be3e41297765c5e661e4683fe432c562b11d085
sluthien <sethegipt@hotmail.com>
2012-02-04 12:35:56 -0300

    primera

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pro::Application.config.secret_token = '00d3c7a61a42d0fc7e66de3e3d5bdde9dfa1a8b546d3a9a405a5423378cc3a3bd063f205bba9a4c3591a007628d3c43cfe8828498e5f6c98bd327c9a4a85a738'
